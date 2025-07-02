d3564faa6d9c282c85e4839236027c14917cdeb2
Callax <pedro.ferreira_farinha@outlook.pt>
2025-07-02 13:36:50 +0100

    cenasboy

--- models/Session.js
+++ models/Session.js
@@ -1,7 +1,7 @@
 const mongoose=require("mongoose");
 
 const sessionSchema=new mongoose.Schema({
-    userId:{type:mongoose.Schema.Types.ObjectId, ref:"USer"},
+    userId:{type:mongoose.Schema.Types.ObjectId, ref:"User"},
 });
 
 module.exports=mongoose.model("Session",sessionSchema);
